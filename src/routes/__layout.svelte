f6c2938a1b5094b052d9257f4884a32bdee7a399
Cullen Champagne <github@cullenchampagne.dev>
2022-01-08 19:08:55 -0600

    initial template

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script context="module" lang="ts">
+    import '../styles/tailwind-output.css'
+
+    /**
+     * This function runs before the component is created and runs both during 
+     * server-side rendering and in the client. Used to fetch the categories from
+     * firebase and have them loaded in the Navigation bar
+     */
+    export async function load() { return {} }
+</script>
+
+<slot />
